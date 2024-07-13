6bd617d3444c8af4f22b82e6988dd17a1b69e8ad
TopsRiddhesh <riddheshpardeshi99816@gmail.com>
2024-07-13 08:02:11 +0530

    Q19 & Q20 done

---
+++ SE/Assignments/Module 1 (SE – Overview of IT Industry)/Q19.c
@@ -0,0 +1,5 @@
+// What is DFD?
+
+/*Ans:
+DFD stands for "Data Flow Diagram." It is a graphical representation of the flow of data within a system, depicting how data moves through processes, stores, and flows between entities. DFDs are commonly used in system analysis and design to visualize the data processing steps and interactions within a system or between systems.
+*/
---
+++ SE/Assignments/Module 1 (SE – Overview of IT Industry)/Q20.c
@@ -0,0 +1,5 @@
+// What is Flowchart?
+
+/*Ans:
+A flowchart is a visual representation of a process or algorithm, typically using standardized symbols and arrows to show the sequence of steps and the flow of inputs and outputs. Flowcharts are widely used in various fields such as software development, business process mapping, education, and engineering.
+*/
